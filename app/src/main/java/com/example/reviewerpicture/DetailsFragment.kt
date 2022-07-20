d09502dc341de826040ad1e0f635583635a08385
rohit <rohitdevnath@mpaani.com>
2022-07-20 23:51:25 +0530

    added android navigation with two fragments

---
+++ app/src/main/java/com/example/reviewerpicture/DetailsFragment.kt
@@ -0,0 +1,41 @@
+package com.example.reviewerpicture
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.reviewerpicture.databinding.FragmentDetailsBinding
+
+
+class DetailsFragment : Fragment() {
+    private var param1: String? = null
+
+    lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_USER_INPUT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentDetailsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvText.text = param1
+    }
+
+    companion object {
+        const val ARG_USER_INPUT = "user_input"
+    }
+}
